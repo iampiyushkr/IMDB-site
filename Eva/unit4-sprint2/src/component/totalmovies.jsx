fdac6728258ffd85cc5bd448e6390a9f1a92cc7b
piyush <iampiyushkr@gmail.com>
2021-08-09 16:24:15 +0530

    check

--- Eva/unit4-sprint2/src/component/totalmovies.jsx
+++ Eva/unit4-sprint2/src/component/totalmovies.jsx
@@ -1,11 +1,16 @@
 import { useSelector } from "react-redux";
-
+import "./ComCss.css"
 export function TotalMovies() {
     const state = useSelector(state => state.payload)
         console.log(state)
     return <>
-        {state.map(e => <div>
-            <p>{e.name }</p>
-      </div>)}
+        <div className="mainDiv">
+        {state.map(e => <div >
+            
+                <img src={ e.img} alt="Done"/>
+            <p>Title: {e.name}</p>
+            <p>IMDB: { e.imdb}</p>
+                </div>)}
+            </div>
     </>
 }
